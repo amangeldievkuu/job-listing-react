d5c6b7c7e2f4b3fda32c7a9c2e76cc0e2f92580f
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-04 13:13:38 +0800

    create ViewAllJobs

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,16 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
 const App = () => {
   return (
     <>
       <Navbar />
-      <Hero
-        title="Become a Frontend Developer"
-        subTitle="Find the React job that fits your skills and needs"
-      />
+      <Hero />
       <HomeCards />
       <JobListings />
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };
