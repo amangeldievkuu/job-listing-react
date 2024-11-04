aa03ce85d60f89fd5ae20943e5f95272f2639727
amangeldievkuu <amangeldievkuu@gmail.com>
2024-11-04 13:20:41 +0800

    create state to view the full description

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { useState } from "react";
 
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescriptioin] = useState(false);
+  let description = job.description;
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -9,7 +14,7 @@ const JobListing = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
